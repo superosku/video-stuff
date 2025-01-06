095f92eeefc18965811c0b67f5ef31d49988ef48
Oskari Lehto <oskari.lehto@gmail.com>
2025-01-06 15:32:45 +0200

    Add (an adhoc) fix for clipping mask skewing rotations

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class WaterFlask(VGroup):
         self.big_clipping_mask.move_to(bottom_rectangle_pos + UP * (1.5 + fill_amount))
         self.big_clipping_mask.set_fill(PURPLE, 0.2)
         self.big_clipping_mask.set_stroke(width=0)
-        self.add(self.big_clipping_mask)
+        # self.add(self.big_clipping_mask)
 
         def get_clipping_mask_updater(invisible_rectangle):
             def update_clipping_mask(x):
@@ -78,20 +78,34 @@ class WaterTests(Scene):
     def construct(self):
         flask1 = WaterFlask([RED, YELLOW, YELLOW, BLUE], 2)
         flask1.shift(RIGHT * 1)
+        flask1.big_clipping_mask.shift(RIGHT * 1)
+        self.add(flask1.big_clipping_mask)
         self.add(flask1)
 
         flask2 = WaterFlask([RED, GREEN, BLUE, YELLOW], 4)
         flask2.shift(LEFT * 1)
+        flask2.big_clipping_mask.shift(LEFT * 1)
         self.add(flask2)
+        self.add(flask2.big_clipping_mask)
 
         self.wait()
 
         for i in range(2):
+            flask2.big_clipping_mask.shift(-flask2.get_center())
+            flask2.big_clipping_mask.rotate_about_origin(BOTTLE_ROTATION)
+            flask2.big_clipping_mask.shift(flask2.get_center())
+            flask2.big_clipping_mask.shift(UP + RIGHT)
+
             self.play(
                 flask2.animate.shift(UP + RIGHT).rotate(BOTTLE_ROTATION)
             )
 
             self.play(flask2.animate_empty(1), flask1.animate_fill(1))
+
+            flask2.big_clipping_mask.shift(-flask2.get_center())
+            flask2.big_clipping_mask.rotate_about_origin(-BOTTLE_ROTATION)
+            flask2.big_clipping_mask.shift(flask2.get_center())
+            flask2.big_clipping_mask.shift(DOWN + LEFT)
 
             self.play(
                 flask2.animate.shift(DOWN + LEFT).rotate(-BOTTLE_ROTATION)
